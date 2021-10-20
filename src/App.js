71542bcb55a86c7a30dc1edd87809bc0bfd5d616
GW <kowonp@gmail.com>
2021-10-20 21:28:04 +0900

    Dark mode built with React & React Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Link, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import theme from "./theme";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  body{
+    background-color: ${(props) => (props.isDark ? props.theme.darkColor : props.theme.lightColor)};
+    color: ${(props) => (props.isDark ? props.theme.lightColor : props.theme.darkColor)};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    text-align:center;
+    font-family: 'Henny Penny', cursive;
+  }
+
+  a{
+    color: ${(props) => (props.isDark ? props.theme.lightColor : props.theme.darkColor)};
+    margin: 0 10px;
+    font-size: 25px;
+  }
+`;
+
+const FooterTitle = styled.h5`
+  font-size: 12px;
+  margin-top: 20px;
+`;
+
+const App = ({ state }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle isDark={state} />
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <FooterTitle>&copy; ${new Date().getFullYear()} GW. ALL RIGHTS RESERVED.</FooterTitle>
+      </ThemeProvider>
+    </HashRouter>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  return { state };
+};
+
+export default connect(mapStateToProps, null)(App);
